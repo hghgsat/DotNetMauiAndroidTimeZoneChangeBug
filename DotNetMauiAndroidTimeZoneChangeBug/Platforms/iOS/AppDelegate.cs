c202e40b3ce2a86fa9b7b4ca70647507a166d68b
Harry Gergits <hg@hgs.at>
2023-06-22 10:22:09 +0200

    Projektdateien hinzufügen.

---
+++ DotNetMauiAndroidTimeZoneChangeBug/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DotNetMauiAndroidTimeZoneChangeBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
